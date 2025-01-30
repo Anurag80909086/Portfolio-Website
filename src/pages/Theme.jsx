9813fdcc85d52043dc2a61eb76823314f8f560de
Anurag80909086 <anurag.singh26@amity.edu>
2025-01-30 17:16:58 +0530

    first commit

---
+++ src/pages/Theme.jsx
@@ -0,0 +1,11 @@
+import "../styles/Theme.css";
+
+const Theme = () => {
+  return (
+    <div class="theme-container" id="themeToggle">
+      <i class="fa-solid fa-sun"></i>
+    </div>
+  );
+};
+
+export default Theme;
